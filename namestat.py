5bef60d65e5d91d795503404cd6747ad24edc475
Vladimir <veffhz@gmail.com>
2018-07-20 13:22:10 +0300

    Add part speech choice feature

--- namestat.py
+++ namestat.py
@@ -7,9 +7,14 @@ from nltk import pos_tag
 import helpers
 
 
-def word_is_verb(word):
+def word_is_current_type(word, _type):
     tag = pos_tag([word])[0][1]
-    return word is not None and re.match(r'V[BDGNPZ]', tag)
+    if word is None:
+        return False
+    if _type == 'verb':
+        return re.match(r'V[BDGNPZ]', tag)
+    elif _type == 'noun':
+        return re.match(r'NN', tag)
 
 
 def get_trees_in_specified_path(path):
@@ -32,8 +37,8 @@ def get_functions_names_in_tree(tree):
     return [node.name.lower() for node in ast.walk(tree) if isinstance(node, ast.FunctionDef)]
 
 
-def get_verbs_from_function_name(function_name):
-    return [word for word in function_name.split('_') if word_is_verb(word)]
+def get_part_speech_from_function_name(function_name, _type):
+    return [word for word in function_name.split('_') if word_is_current_type(word, _type)]
 
 
 def get_names_on_function_handle_to_path(path, func):
@@ -47,9 +52,9 @@ def get_all_py_names_in_path(path):
     return helpers.flatten_list([helpers.split_case_name(name) for name in names])
 
 
-def get_top_py_functions_verbs_in_path(path, top_size):
+def get_top_functions_part_speech(path, top_size, _type):
     functions_names = get_names_on_function_handle_to_path(path, get_functions_names_in_tree)
-    verbs = helpers.flatten_list([get_verbs_from_function_name(name) for name in functions_names])
+    verbs = helpers.flatten_list([get_part_speech_from_function_name(name, _type) for name in functions_names])
     return helpers.most_common(verbs, top_size)
 
 
@@ -61,6 +66,7 @@ def get_top_py_functions_names_in_path(path, top_size):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--size', help='Top size for get verbs', type=int, default=200)
+    parser.add_argument('-ps', '--part_speech', help='Part of speech (verb or noun)', default='verb')
     required_source = parser.add_mutually_exclusive_group(required=True)
     required_source.add_argument('-p', '--project_path', help='Project path for explore')
     required_source.add_argument('-r', '--source_repo', help='Source project')
@@ -77,10 +83,12 @@ if __name__ == "__main__":
         path_to_repo = helpers.get_parent_path(source_repo)
         helpers.get_source_repo(source_repo, path_to_repo)
 
-    result_verbs = get_top_py_functions_verbs_in_path(path_to_repo, args.size)
+    size = args.size
+    _type = args.part_speech
+    results = get_top_functions_part_speech(path_to_repo, size, _type)
 
-    total_verbs = sum([vb[1] for vb in result_verbs])
-    print('total %s verbs, %s unique' % (total_verbs, len(set(result_verbs))))
+    total_verbs = sum([vb[1] for vb in results])
+    print('total %s verbs, %s unique' % (total_verbs, len(set(results))))
 
-    for _verb, _occurence in helpers.most_common(result_verbs, args.size):
+    for _verb, _occurence in helpers.most_common(results, args.size):
         print(_verb, _occurence)
--- test_lib.py
+++ test_lib.py
@@ -18,10 +18,16 @@ class TestUM(unittest.TestCase):
         self.assertEqual(8, len(set(functions_names)))
 
     def test_top_functions_verbs_in_path(self):
-        verbs = namestat.get_top_py_functions_verbs_in_path(self.path, 10)
+        verbs = namestat.get_top_functions_part_speech(self.path, 10, 'verb')
         total_verbs = [vb[1] for vb in verbs]
         self.assertEqual(8, sum(total_verbs))
         self.assertEqual(7, len(set(verbs)))
+
+    def test_top_functions_noun_in_path(self):
+        nouns = namestat.get_top_functions_part_speech(self.path, 10, 'noun')
+        total_nouns = [noun[1] for noun in nouns]
+        self.assertEqual(1, sum(total_nouns))
+        self.assertEqual(1, len(set(nouns)))
 
     def test_all_names_in_path(self):
         names = namestat.get_all_py_names_in_path(self.path)
